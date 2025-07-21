13ea8c51ffa3e70d80d4fc9131281bfd1cb75a4b
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-07-21 16:59:02 -0700

    final 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LeadsComponent } from './leads/leads.component';
+import { CompaniesComponent } from './companies/companies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CustomersComponent,
+    LeadsComponent,
+    CompaniesComponent
   ],
   imports: [
     BrowserModule,
