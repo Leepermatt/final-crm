e9e8ef4e8b2c49aac417b49c979d38ee2e8dfcf8
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-07-21 17:05:20 -0700

    final 1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LeadsComponent } from './leads/leads.component';
+import { CompaniesComponent } from './companies/companies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'leads', component: LeadsComponent },
+  { path: 'companies', component: CompaniesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
